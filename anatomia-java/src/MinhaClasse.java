32d66e0e868ee9faff8c636934df1c52b8519bb1
RogerioTiuma <rogeriotiuma@hotmail.com>
2023-01-23 13:32:19 -0300

    Atualização 23_01_2023

--- anatomia-java/src/MinhaClasse.java
+++ anatomia-java/src/MinhaClasse.java
@@ -1,5 +1,16 @@
 public class MinhaClasse{
     public static void main(String[] args) {
-        System.out.print ("Olá turma, sejam bem vindos.");
+        String nome1= "Rogério";
+        String nome2= "Tiúma";
+
+        String nomeCompleto = nomeCompleto (nome1, nome2);
+
+        System.out.print(nomeCompleto);
+
+
+    }
+
+    public static String nomeCompleto (String nome1, String nome2){
+        return "Resultado do método " + nome1.concat(" ").concat(nome2);
     }
 }
