c6c85a5894af958667672abed3bb0f99dff4935b
RogerioTiuma <rogeriotiuma@hotmail.com>
2023-02-10 07:27:20 -0300

    Atualização Metodos

---
+++ EstruturasDeRepetição&Arrays/src/MaiorMedia.java
@@ -0,0 +1,23 @@
+
+
+import java.util.Scanner;
+
+public class MaiorMedia {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numero;
+        int maior=0;
+        int contador=0;
+
+        do{
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+            contador++;
+            if(numero > maior) maior = numero;
+
+        }while(contador <5);
+        System.out.println("O maior é: " + maior);
+    }
+}
