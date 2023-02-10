21f5f5622e7676b3552b6c6018c23858e5ff2304
RogerioTiuma <rogeriotiuma@hotmail.com>
2023-02-10 07:28:56 -0300

    Atualização Metodos

--- EstruturasDeRepetição&Arrays/src/NomeIdade.java
+++ EstruturasDeRepetição&Arrays/src/NomeIdade.java
@@ -1,4 +1,8 @@
-
+/*Faça um programa que leia conjuntos de dois valores,
+ * o primeiro representando o nome do aluno e o segundo 
+ * representando a sua idade. ( Pare o programa inserindo o 
+ * valor 0 no campo nome)
+ */
 import java.util.Scanner;
 
 public class NomeIdade {
